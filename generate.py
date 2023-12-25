47093d0e4f9f4532a5ab3ee09f257ff118963adb
csric <2905491763@qq.com>
2023-12-25 03:54:24 -0500

    argparse and debug

--- generate.py
+++ generate.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import argparse
 
 def create_isomorphic_keyboard(start_note, rows, cols, upper_increase = 5):
     # Define the notes and semitones
@@ -15,7 +16,7 @@ def create_isomorphic_keyboard(start_note, rows, cols, upper_increase = 5):
         keyboard_row = []
         for col in range(cols):
             # Calculate the semitone index for this key
-            semitone_index = start_semitone_index + col - row * upper_increase
+            semitone_index = start_semitone_index + col + row * upper_increase
             octave, note_index = divmod(semitone_index, 12)
             note_name = notes[note_index]
             # Append the note with octave
@@ -32,17 +33,31 @@ def highlight_c_notes(val):
     return f'<span style="color: {color}">{val}</span>'
 
 
-keyboard_layout = create_isomorphic_keyboard("C2", 16, 16, 5)
 
-# Display the layout
-keyboard_layout = keyboard_layout.iloc[::-1]
-print(keyboard_layout)
-keyboard_layout.to_csv("keyboard_layout.csv", index=False)
+if __name__ == "__main__":
+    # 创建命令行解析器
+    parser = argparse.ArgumentParser(description="Create an Isomorphic Keyboard Layout")
+    parser.add_argument("--start-note", type=str, help="Starting note, e.g., 'C2'")
+    parser.add_argument("--rows", type=int, help="Number of rows in the keyboard layout")
+    parser.add_argument("--cols", type=int, help="Number of columns in the keyboard layout")
+    parser.add_argument("--upper-increase", type=int, help="Number of semitones to increase per row")
+    
+    args = parser.parse_args()
+    
+    keyboard_layout = create_isomorphic_keyboard(args.start_note, args.rows, args.cols, args.upper_increase)
+    keyboard_layout = keyboard_layout.iloc[::-1]
+    # Display the layout
+    print(keyboard_layout)
+    
+    base_filename = f"keyboard_layout_{args.start_note}_{args.rows}x{args.cols}_increase{args.upper_increase}"
+    csv_filename = base_filename + ".csv"
+    html_filename = base_filename + ".html"
 
-html_keyboard = keyboard_layout.to_html(escape=False, formatters=[highlight_c_notes]*keyboard_layout.shape[1])
+    keyboard_layout.to_csv(csv_filename, index=False)
+    html_keyboard = keyboard_layout.to_html(escape=False, formatters=[highlight_c_notes]*keyboard_layout.shape[1])
+    with open(html_filename, 'w') as file:
+        file.write(html_keyboard)
 
-html_keyboard_file_path = "keyboard_layout.html"
+    print(f"Files generated: {csv_filename}, {html_filename}")
 
-with open(html_keyboard_file_path, 'w') as file:
-    file.write(html_keyboard)
-
+# python .\generate.py --start-note C2 --rows 16 --cols 16 --upper-increase 5
